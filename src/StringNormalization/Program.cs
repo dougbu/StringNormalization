4d6da79a6ba1ede5b767e55d35910904d72c51ea
Doug Bunting <dougbu@microsoft.com>
2015-09-26 00:32:28 -0700

    Initial commit

---
+++ src/StringNormalization/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringNormalization
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var penguin = "pingüino";
+
+            Console.WriteLine(penguin.IsNormalized());
+            Console.WriteLine(penguin.Normalize());
+        }
+    }
+}
